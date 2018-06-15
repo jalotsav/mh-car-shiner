740f0a2db5c3eb9bc031827dac72184d5fdfa7fc
Jaldeep Asodariya <jdpatel.android@gmail.com>
2018-06-15 18:00:28 +0530

    Add Profile menu - Implement UI design and code for PROFILE menu. Get data from SharedPreferences and set on UI.(Edit Profile - Show "In Development" message).

--- app/src/main/java/com/jalotsav/mhcarshiner/nvgtnvwmain/FrgmntProfile.java
+++ app/src/main/java/com/jalotsav/mhcarshiner/nvgtnvwmain/FrgmntProfile.java
@@ -19,19 +19,59 @@ package com.jalotsav.mhcarshiner.nvgtnvwmain;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.jalotsav.mhcarshiner.R;
+import com.jalotsav.mhcarshiner.common.UserSessionManager;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Jalotsav on 6/12/2017.
  */
 
 public class FrgmntProfile extends Fragment {
+
+    @BindView(R.id.cordntrlyot_frgmnt_profile) CoordinatorLayout mCrdntrlyot;
+
+    @BindView(R.id.tv_frgmnt_profile_firstcharname) TextView mTvFirstCharName;
+    @BindView(R.id.tv_frgmnt_profile_fullname) TextView mTvFullName;
+    @BindView(R.id.tv_frgmnt_profile_mobile) TextView mTvmobile;
+    @BindView(R.id.tv_frgmnt_profile_email) TextView mTvEmail;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+
+        View rootView = inflater.inflate(R.layout.lo_frgmnt_profile, container, false);
+        ButterKnife.bind(this, rootView);
+
+        UserSessionManager session = new UserSessionManager(getActivity());
+        mTvFirstCharName.setText(session.getFirstName().substring(0,1).toUpperCase());
+        mTvFullName.setText(session.getFirstName().concat(" ").concat(session.getLastName()));
+        mTvmobile.setText(session.getMobile());
+        mTvEmail.setText(session.getEmail());
+
+        return rootView;
+    }
+
+    @OnClick({R.id.fab_frgmnt_profile_edit})
+    public void onClickView(View view) {
+
+        switch (view.getId()) {
+            case R.id.fab_frgmnt_profile_edit:
+
+                Toast.makeText(getActivity(), "The work is under way.", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
